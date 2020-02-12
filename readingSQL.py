2c42f990fa383e3cbe11b36b0e44260602a6bb56
Arnaud D <arnaud.delol@gmail.com>
2020-02-12 14:33:01 +0100

    Update readingSQL.py

--- readingSQL.py
+++ readingSQL.py
@@ -16,6 +16,14 @@ from collections import Counter
 import json
 import string
 
+#https://spacy.io/usage
+import spacy
+nlp = spacy.load('fr_core_news_sm')
+
+
+from nltk.corpus import stopwords
+list_stopwords = stopwords.words('french')
+
 '''
 Prends un tweet en entree de chaque fonction et le renvoie transforme
 
@@ -41,8 +49,12 @@ def stem(tweet):
     tweet = " ".join(stemmer.stem(word) for word in tweet.split(" "))
     return stemmer.stem(tweet)
 
-def lemmatise(tweet):
+# a tester pour comparer avec le stem, il met hashtag dans un token propre
+def lemmatize(tweet):
+    doc = nlp(tweet)
+    tweet =  " ".join(token.lemma_ for token in doc)
     return tweet
+    
 
 def removeMentions(tweet):
     tweet = re.sub(r'@[A-Za-z0-9_]+', '', tweet)
@@ -53,11 +65,18 @@ def removeNumbers(tweet):
     return tweet
 
 
+def listHashtags(tweet):
+    hashtags = re.findall(r'#\S+',tweet)
+    return hashtags
 
 def removeSparseTerms(dicti,nbTerms):
     return 
 
-cnx = mysql.connector.connect(user='root', password='pldac',
+
+#password :
+#pldac
+#root
+cnx = mysql.connector.connect(user='root', password='root',
                               host='127.0.0.1',
                               database='pldac')
 cursor = cnx.cursor()
@@ -73,16 +92,45 @@ tweets['text'] = tweets['text'].apply(removePunctuation)
 tweets['text'] = tweets['text'].apply(removeNumbers)
 tweets['text'] = tweets['text'].apply(lowerCase)
 tweets['text'] = tweets['text'].apply(stem)
+#tweets['text'] = tweets['text'].apply(lemmatize)
+
+
 
 print(tweets)
+''' (?u) = re.UNICODE ,\b = delimiteur d un mot
+la regex permet de garder aussi les hashtags dans le passage dans countVectorizer
 
-vectorizer = CountVectorizer()
+'''
+vectorizer = CountVectorizer(token_pattern= r'(?u)#?\b\w\w+\b',stop_words=list_stopwords)
 X = vectorizer.fit_transform(tweets['text'])
 print(vectorizer.get_feature_names())
 print(X.toarray())
+features = vectorizer.get_feature_names()
+'''
+Histogramme des mots cles (les plus souvents utilisés)
+Addition des lignes de la matrice 
+ordre decroissant
+garder que les n mots les plus utilises
+'''
+sum_rows = np.sum(X,axis=0)
 
-counter = Counter(tweets['text'])
-print()
+sortOrder = (-sum_rows).argsort()
+print(sortOrder)
+print(sum_rows.shape)
+features = np.array(features)
+print(features.shape)
+sum_rows = np.reshape(sum_rows,features.shape)
+print(len(sum_rows[0]))
+sum_rows = sum_rows[sortOrder]
+features = features[sortOrder]
+print(sum_rows)
+print(features)
+
+'''
+Classifier tweets selon mots clés
+'''
+
+
 
 cursor.close()
 cnx.close()
